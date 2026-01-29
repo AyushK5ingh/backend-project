bcc759ed923e06b0217f3e8fbd5e5136910ee57e
AyushK5ingh <ayushkumarm.singh@gmail.com>
2026-01-29 08:41:50 +0530

    commentsing

--- src/midlewares/auth.middleware.js
+++ src/midlewares/auth.middleware.js
@@ -1,25 +1,31 @@
 import { ApiError } from "../utils/apierror";
 import { asyncHandler } from "../utils/asynchandler";
-import jwt from "jsonwebtoken"
-import {User} from "../models/user.model"
-export const verifyJWT = asyncHandler(async(req, res, next)=>{
-    try {
-        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ","")
-        if(!token){
-            throw new ApiError(401,"Unauthorized request")
-        }
-    
-        const decodedtoken = jwt.verify(token, proccess.env.ACCESS_TOKEN_SECRET)
-        const user = await User.findById(decodedtoken._id).select("-password","-refreshToken")
-    
-        if(!user){
-            throw new ApiError(401,"Invalid Access Token")
-        }
-        
-        req.user=user;
-        next()
-    
-    } catch (error) {
-        throw new ApiError(401, error?.message || "Invalid accesstoken")
+import jwt from "jsonwebtoken";
+import { User } from "../models/user.model";
+// can write _ if res is not used
+
+export const verifyJWT = asyncHandler(async (req, _, next) => {
+  try {
+    const token =
+      req.cookies?.accessToken ||
+      req.header("Authorization")?.replace("Bearer ", "");
+    if (!token) {
+      throw new ApiError(401, "Unauthorized request");
     }
+
+    const decodedtoken = jwt.verify(token, proccess.env.ACCESS_TOKEN_SECRET);
+    const user = await User.findById(decodedtoken._id).select(
+      "-password",
+      "-refreshToken"
+    );
+
+    if (!user) {
+      throw new ApiError(401, "Invalid Access Token");
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    throw new ApiError(401, error?.message || "Invalid accesstoken");
+  }
 });
